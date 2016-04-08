b753d3806a97488d7501665cec321cdae7e95614
BunFongUng <bunfong.ung@gmail.com>
2016-04-08 09:53:27 +0700

    file update

--- admin/menu/update.php
+++ admin/menu/update.php
@@ -65,7 +65,7 @@
                                         <?php if($stmt3->rowCount() > 0):?>
                                             <option value="<?= $row3['ID']?>"><?= $row3['MenuTitle']?></option>
                                         <?php else:?>
-                                            <option value=""></option>
+                                            <option value="0">None</option>
                                         <?php endif;?>
                                     </select>
                                 </div>
