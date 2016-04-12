a22db2dece6c2d195ed803a8df78887e2f73c197
BunFongUng <bunfong.ung@gmail.com>
2016-04-12 09:12:40 +0700

    databse file

---
+++ database/menus.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menus` (`ID`, `MenuTitle`, `ParentMenu`) VALUES (1, 'testing', NULL);
+INSERT INTO `menus` (`ID`, `MenuTitle`, `ParentMenu`) VALUES (2, 'testing', NULL);
+INSERT INTO `menus` (`ID`, `MenuTitle`, `ParentMenu`) VALUES (7, 'About Us', '0');
+INSERT INTO `menus` (`ID`, `MenuTitle`, `ParentMenu`) VALUES (8, 'Contact Us', '7');
---
+++ database/users.sql
@@ -0,0 +1,1 @@
+INSERT INTO `users` (`id`, `email`, `password`, `userStatus`, `tokenCode`) VALUES (1, 'bun.fong2009@gmail.com', 'd262341de07d535753222a1365e34b98', 'Y', 'a3cecb37dd888367f5bfa1b070decb86');
