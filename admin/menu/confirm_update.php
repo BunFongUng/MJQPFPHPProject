8d37d6271e9871bbc758cee99ea6fdc9b37a71b9
Bun Fong <bunfong@Buns-Mac-mini.local>
2016-04-13 08:37:07 +0700

    adding new code

--- admin/menu/confirm_update.php
+++ admin/menu/confirm_update.php
@@ -11,9 +11,29 @@ try {
 
     //if the user click the update button
     if(isset($_POST['update'])) {
+        $error_message = array();
         $update_id = (isset($_POST['update_id'])) ? $_POST['update_id'] : '';
         $title = (isset($_POST['title']) && !empty($_POST['title'])) ? $_POST['title'] : '';
-        $parent = (isset($_POST['parent']) && !empty($_POST['parent'])) ? $_POST['parent'] : '';
+        $parent = (isset($_POST['parent'])) ? $_POST['parent'] : '';
+
+        if($title == "") {
+            $error_message["title_required"] = "Title Field Is Required!";
+        }
+
+        if(empty($error_message)) {
+            $database = new Database();
+            $conn = $database->getConnection();
+            $sql_update = "UPDATE menus SET MenuTItle = :title, ParentMenu = :parent WHERE ID = :update_id";
+            $stmt = $conn->prepare($sql_update);
+            $stmt->bindParam(":title", $title);
+            $stmt->bindParam(":parent", $parent);
+            $stmt->bindParam(":update_id", $update_id);
+            $stmt->execute();
+
+            if($stmt->rowCount() > 0) {
+                echo "Record has been updated!";
+            }
+        }
     }
 
 } catch(PDOException $err) {
