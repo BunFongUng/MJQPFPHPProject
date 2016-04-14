2cf8a304756f0c3c7c3370861ff7fd495576b5a3
Bun Fong <bunfong@Buns-Mac-mini.local>
2016-04-14 08:41:15 +0700

    update code in menu section

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ try {
         global $menus;
         echo "<ul>";
             foreach($main_menus as $menu_id => $title) {
-                echo "<li><a href='#'>{$title}</a>";
+                echo "<li><a href='".$title.".php'>{$title}</a>";
                     if(isset($menus[$menu_id])) {
                         listing_menus($menus[$menu_id]);
                     }
